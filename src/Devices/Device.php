eba4af34babc83c534f63f2916779dd5096fe0c7
Craig Duncan <git@duncanc.co.uk>
2014-12-30 16:29:45 +0000

    Create Spotify resource classes

--- src/Devices/Device.php
+++ src/Devices/Device.php
@@ -115,6 +115,7 @@ final class Device implements DeviceInterface
                 break;
             case "AlarmClock":
             case "DeviceProperties":
+            case "MusicServices":
             case "ZoneGroupTopology":
                 $path = null;
                 break;
