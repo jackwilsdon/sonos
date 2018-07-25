6652d1ab211a6ce8dd7501b58b3a94dab85ae647
Craig Duncan <git@duncanc.co.uk>
2018-07-25 23:22:10 +0100

    soap

--- tests/local/router.php
+++ tests/local/router.php
@@ -4,25 +4,37 @@ use duncan3dc\DomParser\XmlParser;
 
 require __DIR__ . "/../../vendor/autoload.php";
 
-$path = __DIR__ . $_SERVER["REQUEST_URI"];
+$uri = $_SERVER["REQUEST_URI"];
 
+$path = __DIR__ . $uri;
 if (file_exists($path)) {
     echo file_get_contents($path);
     return;
 }
 
-if (file_exists("{$path}.php")) {
-    require "{$path}.php";
 
-    $xml = file_get_contents("php://input");
-    $parser = new XmlParser($xml);
+$class = "Sonos" . str_replace("/", "\\", $uri);
+$controller = new $class;
 
-    $tag = $parser->getTag("Envelope")->getTag("Body")->childNodes[0];
-    list($ns, $action) = explode(":", $tag->tagName);
-    $options = simplexml_load_string($tag->output());
+$xml = file_get_contents("php://input");
+$parser = new XmlParser($xml);
 
-    $action($options);
-    return;
-}
+$tag = $parser->getTag("Envelope")->getTag("Body")->childNodes[0];
+list($ns, $action) = explode(":", $tag->tagName);
+$options = simplexml_load_string($tag->output());
 
-return false;
+$method = strtolower($action);
+$result = $controller->$method($options);
+
+?>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+    <s:Body>
+        <u:<?= $action; ?>Response xmlns:u="urn:schemas-upnp-org:service:ZoneGroupTopology:1">
+            <?php
+                foreach ($result as $key => $val) {
+                    echo "<{$key}>{$val}</{$key}>\n";
+                }
+            ?>
+        </u:<?= $action; ?>Response>
+    </s:Body>
+</s:Envelope>
